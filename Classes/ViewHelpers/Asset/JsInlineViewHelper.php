ed8363a60e4025339ea330154f9153ac3b0407f9
SvenJuergens <sj@nordsonne.de>
2017-03-23 17:30:35 +0100

    [FEATURE] JsInlineViewhelper can now inline a js file

--- Classes/ViewHelpers/Asset/JsInlineViewHelper.php
+++ Classes/ViewHelpers/Asset/JsInlineViewHelper.php
@@ -21,13 +21,11 @@ use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
 /**
  * ViewHelper to include a inline JS file
  *
- *
- *
  * # Example: Basic example
  * # Include in template
  *
  * <code>
- * <sj:asset.jsInline position="bottom"> ... </sj:asset.jsInline>
+ * <sj:asset.jsInline position="bottom" path="{settings.jsInlineFile}"> ... </sj:asset.jsInline>
  * </code>
  * <output>
  *
@@ -36,23 +34,43 @@ use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
 class JsInlineViewHelper extends AbstractViewHelper
 {
     /**
-     * Include JS Content
-     *
-     * @param string $position Optional
-     * @param boolean $compress Define if file should be compressed
      * @return void
      */
-    public function render($position = null, $compress = true)
+    public function initializeArguments()
     {
-        $content = GeneralUtility::minifyJavaScript($this->renderChildren());
-        $name = md5($content);
+        $this->registerArgument(
+            'position',
+            'string',
+            'Optional: if is "bottom", then js is inserted in Footer',
+            false,
+            null
+        );
+        $this->registerArgument('compress', 'boolean', 'Define if file should be compressed', false, true);
+        $this->registerArgument('path', 'string', 'Path to the JS file which should be included', false, null);
+    }
+
+
+    /**
+     * Include JS Content
+     *
+     * @return void
+     */
+    public function render()
+    {
+        $content = trim($this->renderChildren());
+        if (!is_null($this->arguments['path'])
+            && strtolower(substr($this->arguments['path'], -3)) === '.js'
+        ) {
+            $content .= GeneralUtility::getUrl(GeneralUtility::getFileAbsFileName($this->arguments['path']));
+        }
+        $name = md5(GeneralUtility::minifyJavaScript($content));
 
         /** @var PageRenderer $pageRenderer */
         $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
-        if ($position == 'bottom') {
-            $pageRenderer->addJsFooterInlineCode($name, $content, $compress);
+        if ($this->arguments['position'] == 'bottom') {
+            $pageRenderer->addJsFooterInlineCode($name, $content, $this->arguments['compress']);
         } else {
-            $pageRenderer->addJsInlineCode($name, $content, $compress);
+            $pageRenderer->addJsInlineCode($name, $content, $this->arguments['compress']);
         }
     }
 }
